9a4af1c6ad2fc63ed72edb7ccb1d9b3e4f4d49f8
Ken Ingram <ken.ingram@gmail.com>
2024-05-03 08:51:36 -0700

    Add module for responding to API calls.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors'
 import { routes } from './routes';
 import { initializeDbConnection } from './db';
 
 const PORT = process.env.PORT || 8081;
 
 const app = express();
-
+app.use(cors());
 // This allows us to access the body of POST/PUT
 // requests in our route handlers (as req.body)
 app.use(express.json());
