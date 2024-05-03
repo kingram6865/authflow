9f7b0a6fad8960aa7689bb7f22882077eea2045e
Ken Ingram <ken.ingram@gmail.com>
2024-05-03 08:46:03 -0700

    Refactor to import routes from routes/index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
 import { publicRoutes, privateRoutes } from './routes'
 
 function App() {
