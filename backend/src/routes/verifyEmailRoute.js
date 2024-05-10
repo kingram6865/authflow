c2cfb1166481669a8ce41eab8fdc500b014022da
Ken Ingram <ken.ingram@gmail.com>
2024-05-10 10:40:59 -0700

    Console.log verification data.

--- backend/src/routes/verifyEmailRoute.js
+++ backend/src/routes/verifyEmailRoute.js
@@ -9,10 +9,15 @@ export const verifyEmailRoute = {
     const { verificationString } = req.body
     const db = getDbConnection('react-auth-db')
     const result = await db.collection('users').findOne({ verificationString })
-    console.log(`verificationString: ${result}`)
+
+    console.log(`(verifyEmailRoute)[line 13:] verificationData: \n${JSON.stringify(result, null, 2)}`)
+
     if (!result) return res.status(401).json({ message: "The email verification code is incorrect.", result })
+
     const { _id: id, email, info } = result
+
     await db.collection('users').updateOne({ _id: ObjectId(id)}, { $set: { isVerified: true }});
+
     jwt.sign({ id, email, isVerified: true, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
       if (err) return res.sendStatus(500);
       res.status(200).json({ token })
