56c862b7d2a506c625981d066d4fe517b486633f
Ken Ingram <ken.ingram@gmail.com>
2025-05-27 06:37:38 -0700

    Console log email sent.

--- backend/src/util/sendEmail.js
+++ backend/src/util/sendEmail.js
@@ -1,8 +1,17 @@
 import sendgrid from '@sendgrid/mail'
+// import { Mail } from '@sendgrid/helpers'
+const { classes: { Mail,  },} = require('@sendgrid/helpers');
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 
+function payloadOutput(data) {
+  const mail = Mail.create(data)
+  const body = mail.toJSON()
+  return JSON.stringify(body)
+}
+
 export const sendEmail = ({to, from, subject, text, html}) => {
   const msg = {to, from, subject, text, html};
-  return sendgrid.send(msg)
+  // console.log(payloadOutput(msg))
+  return sendgrid.send(msg);
 }
